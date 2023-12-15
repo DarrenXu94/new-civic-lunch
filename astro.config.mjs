ed2f315eeaa9aafde57cf366d8b3c79482b74e33
Darren Xu <darrenxu1994@gmail.com>
2023-12-15 20:59:16 +1100

    url in config?

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://main--vermillion-custard-f525f4.netlify.app/",
+  site: process.env.URL,
   output: "server",
   adapter: netlify(),
 
