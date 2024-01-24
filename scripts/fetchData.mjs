bb3142e750435c005ff434e6820de5ad39b926b8
Darren Xu <darrenxu1994@gmail.com>
2024-01-24 20:53:38 +1100

    fetch photos

--- scripts/fetchData.mjs
+++ scripts/fetchData.mjs
@@ -32,9 +32,49 @@ const main = async () => {
 
   // console.log(log);
 
+  const resultsList = [log.results[0]];
+  // const resultsList = log.results;
+
   if (log.results) {
-    for (const result of log.results) {
+    for (const result of resultsList) {
       console.log(result.child_page.title);
+      const id = result.id;
+      const page = await getData(id, "page"); // for cover photo only
+      const item = await getData(id); // page content
+
+      const title = result.child_page.title;
+      const titleNoSpace = title.replaceAll(" ", "_");
+
+      const cover =
+        page.data.response?.cover?.external?.url ||
+        page.data.response?.cover.file?.url;
+
+      await axios({
+        method: "get",
+        url: cover,
+        responseType: "stream",
+      }).then(function (response) {
+        // console.log(response.headers["content-type"], "image response");
+
+        if (response.headers["content-type"] === "image/jpeg") {
+          response.data.pipe(
+            fs.createWriteStream(`../public/assets/covers/${titleNoSpace}.jpeg`)
+          );
+        }
+      });
+
+      // let content = "";
+      const arrayContent = [];
+      for (let block of item.data.response.results) {
+        // console.log(block);
+        // content += block.paragraph?.rich_text[0]?.plain_text;
+        const plainText = block.paragraph?.rich_text[0]?.plain_text;
+        if (plainText) {
+          arrayContent.push(block.paragraph?.rich_text[0]?.plain_text);
+        }
+      }
+
+      // console.log(cover, arrayContent);
     }
   }
 };
