5de03819e0daa3bb00e655ef72455325bb9cc13d
Darren Xu <darrenxu1994@gmail.com>
2024-07-18 21:36:33 +1000

    Ratings page

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,13 @@
 export const SITE_TITLE = "Civic lunch";
 export const SITE_DESCRIPTION =
   "CIVIC LUNCH is a HOT new lunch food review website!";
+
+export const RATING_ARRAY = ["S", "A", "B", "C", "F"];
+
+export const RATING_DESCRIPTIONS = {
+  S: "Diners in this category offer an exceptional dining experience, impeccable service, and a special X factor.",
+  A: "Diners with an 'A' rating provide high-quality food and service. While they may not be the pinnacle, they consistently impress and are frequently on the rotational menu.",
+  B: "A 'B' rated diner offers a satisfying dining experience. They are dependable but not preferable.",
+  C: "Diners with a 'C' rating are below average. They may have a few redeeming qualities, but they are not recommended.",
+  F: "Diners with an 'F' rating have wronged me in some way. I hope to never give out an F rating.",
+};
