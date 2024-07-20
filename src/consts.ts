15059c97bb4b7e0208a6fcced1e0d8d6159514b1
Darren Xu <darrenxu1994@gmail.com>
2024-07-20 11:22:01 +1000

    Mobile table

--- src/consts.ts
+++ src/consts.ts
@@ -8,9 +8,9 @@ export const SITE_DESCRIPTION =
 export const RATING_ARRAY = ["S", "A", "B", "C", "F"];
 
 export const RATING_DESCRIPTIONS = {
-  S: "Diners in this category offer an exceptional dining experience, impeccable service, and a special X factor.",
-  A: "Diners with an 'A' rating provide high-quality food and service. While they may not be the pinnacle, they consistently impress and are frequently on the rotational menu.",
-  B: "A 'B' rated diner offers a satisfying dining experience. They are dependable but not preferable.",
-  C: "Diners with a 'C' rating are below average. They may have a few redeeming qualities, but they are not recommended.",
-  F: "Diners with an 'F' rating have wronged me in some way. I hope to never give out an F rating.",
+  S: "Restaurants in this category offer an exceptional dining experience, impeccable service, and a special X factor.",
+  A: "Restaurants with an 'A' rating provide high-quality food and service. While they may not be the pinnacle, they consistently impress and are frequently on the rotational menu.",
+  B: "A 'B' rated restaurant offers a satisfying dining experience. They are dependable but not preferable.",
+  C: "Restaurants with a 'C' rating are below average. They may have a few redeeming qualities, but they are not recommended.",
+  F: "Restaurants with an 'F' rating have wronged me in some way. I hope to never give out an F rating.",
 };
